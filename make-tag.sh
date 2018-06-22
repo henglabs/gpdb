e1b105901c2138376e0bf1b3e078dac9aa7c3535
weishishuo <weishishuo329@gmail.com>
2018-06-22 11:19:45 +0800

    [henglabs/everest#1496] add make tag script

---
+++ make-tag.sh
@@ -0,0 +1,19 @@
+set -e
+if [ $# -ne 1 ];then
+    echo "bash $0 tagname(like 5.3.0.2)"
+    exit 1
+fi
+TAG="$1"
+SCRIPT_DIR=`dirname $0`
+SCRIPT_DIR=$(cd ${SCRIPT_DIR};pwd)
+cd ${SCRIPT_DIR}
+TMP_BRANCH="tmp-$$"
+git checkout -b ${TMP_BRANCH}
+echo "${TAG}" >hengshi-build/tagname
+git add hengshi-build/tagname
+git commit -m "make tag ${TAG}"
+git tag ${TAG}
+git push origin ${TAG}
+git checkout ${TAG}
+git branch -d ${TMP_BRANCH}
+git checkout hs-5.3.0-master
